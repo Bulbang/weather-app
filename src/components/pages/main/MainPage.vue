c35c0370179802755379fa2f79e76719e84bc37a
Roman <nabukhotnyiroman@gmail.com>
2023-01-01 22:46:10 +0200

    feat: Add chart

--- src/components/pages/main/MainPage.vue
+++ src/components/pages/main/MainPage.vue
@@ -1,23 +1,61 @@
 <template>
-  <div>{{ weathers }}</div>
+  <div>
+    <ChartLine
+      id="chart"
+      :data="{
+        labels: dates,
+        datasets: [{ data: [40, 20, 12] }],
+      }"
+      :options="chartOptions"
+    />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, computed } from 'vue';
+import { Line } from 'vue-chartjs';
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js';
 import { useWeathersStore } from '../../../stores/weathers';
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
 
 export default defineComponent({
   name: 'MainPage',
+  components: {
+    ChartLine: Line,
+  },
   setup() {
     const weathersStore = useWeathersStore();
-    const weathers = computed(() => weathersStore.weathers);
+    const dates = computed(() => weathersStore.dates);
     return {
       weathersStore,
-      weathers,
+      dates,
     };
   },
   mounted() {
     this.weathersStore.getWeathers();
+  },
+  data() {
+    return {
+      selected: null,
+      chartOptions: {
+        responsive: true,
+      },
+    };
   },
 });
 </script>
--- src/stores/weathers.ts
+++ src/stores/weathers.ts
@@ -11,17 +11,25 @@ interface State {
     avg_humidity: number;
     avg_visibility_km: number;
   }[];
+  dates: string[];
 }
 
 export const useWeathersStore = defineStore('weathers', {
   state: (): State => ({
     weathers: [],
+    dates: [],
   }),
   actions: {
     async getWeathers(daysCount?: number, city?: string) {
       try {
-        const response = await axios.get('');
+        const response = await axios.get('', {
+          params: {
+            daysCount,
+            city,
+          },
+        });
         this.weathers = response.data;
+        this.dates = this.weathers.map((weather) => weather.date);
       } catch (error) {
         console.log(error);
       }
